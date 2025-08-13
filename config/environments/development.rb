52c203246839d3b0b77b72dd4648a3861c0f66be
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-08-13 12:21:48 +0700

    feat(config): setup mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
+  # Set localhost to be used by links generated in mailer templates.
+  config.action_mailer.default_url_options = { host: "localhost", port: 8080 }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV.fetch("MAILTRAP_USERNAME", nil),
@@ -46,9 +49,6 @@ Rails.application.configure do
     port: 2525,
     authentication: :plain
   }
-
-  # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 8080 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
